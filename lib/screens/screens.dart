e189159466c884dd1876d8082df347ad77f33cec
Sadot18 <19161291@itoaxaca.edu.mx>
2025-07-18 20:18:38 -0600

    Añade la exportación de la pantalla de película en screens.dart

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -2,3 +2,4 @@ export 'package:app_widgets/screens/alert_screen.dart';
 export 'package:app_widgets/screens/avatar_screen.dart';
 export 'package:app_widgets/screens/custom_home_page.dart';
 export 'package:app_widgets/screens/listview_screen.dart';
+export 'package:app_widgets/screens/movie_screen.dart';
