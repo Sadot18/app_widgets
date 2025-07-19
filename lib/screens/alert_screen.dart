61a4b36775d961a712331add5f440d682271bf20
Sadot18 <19161291@itoaxaca.edu.mx>
2025-07-18 20:01:21 -0600

    Añade la pantalla de alerta con estructura básica y texto de ejemplo.

---
+++ lib/screens/alert_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AlertScreen extends StatelessWidget {
+  const AlertScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(leading: Text("Alert Screen")),
+      body: Center(child: Text('This is the Alert screen!')),
+    );
+  }
+}
