01eb3a192c13a6e317ef1b12b85064418124a20c
Sadot18 <19161291@itoaxaca.edu.mx>
2025-07-18 22:39:38 -0600

    Reestructura ListviewScreen para mostrar una lista de personajes con ListView.separated

--- lib/screens/listview_screen.dart
+++ lib/screens/listview_screen.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 
 class ListviewScreen extends StatelessWidget {
-  const ListviewScreen({super.key});
+  ListviewScreen({super.key});
+
+  final List<String> personajes = [
+    "Goku",
+    "Vegeta",
+    "Gohan",
+    "Piccolo",
+    "Bulma",
+    "Krillin",
+    "Frieza",
+    "Cell",
+    "Majin Buu",
+    "Trunks",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(leading: Text("ListView")),
-      body: Center(child: Text('This is the List View screen!')),
+      appBar: AppBar(title: Text("ListView")),
+      body: ListView.separated(
+        itemCount: personajes.length,
+        itemBuilder: (_, index) {
+          return ListTile(
+            title: Text(personajes[index], style: TextStyle(fontSize: 20)),
+            leading: Icon(Icons.person),
+            subtitle: Text('Dragonball Z'),
+            trailing: Icon(Icons.arrow_forward_ios, color: Colors.blue),
+          );
+        },
+        separatorBuilder: (_, index) {
+          return Divider();
+        },
+      ),
     );
   }
 }
