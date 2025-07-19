07355b98cbe65c7383775a0be429d95321b0d876
Sadot18 <19161291@itoaxaca.edu.mx>
2025-07-18 22:38:32 -0600

    Mejora el estilo de los iconos y añade color a los ListTiles en CustomHomePage

--- lib/screens/custom_home_page.dart
+++ lib/screens/custom_home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class CustomHomePage extends StatelessWidget {
   const CustomHomePage({super.key});
 
+//TODO: investigar como hacer dinamico el listado de widgets
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,28 +17,32 @@ class CustomHomePage extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
-            leading: Icon(Icons.list),
+            leading: Icon(Icons.list, color: Colors.blue),
             title: Text('ListView Screen'),
             subtitle: Text('El siguiente screen es un listview'),
-            trailing: Icon(Icons.navigate_next, color: Colors.red),
+            trailing: Icon(Icons.navigate_next, color: Colors.red, size: 40,),
+            onTap: () => Navigator.pushNamed(context, 'listview'),
           ),
           ListTile(
-            leading: Icon(Icons.notification_add),
+            leading: Icon(Icons.notification_add, color: Colors.red),
             title: Text('Alert Screen'),
             subtitle: Text('El siguiente screen es un alert'),
-            trailing: Icon(Icons.navigate_next),
+            trailing: Icon(Icons.navigate_next, color: Colors.red, size: 40,),
+            onTap: () => Navigator.pushNamed(context, 'alert'),
           ),
           ListTile(
-            leading: Icon(Icons.supervised_user_circle_outlined),
+            leading: Icon(Icons.supervised_user_circle_outlined, color: Colors.green),
             title: Text('Avatar Screen'),
             subtitle: Text('El siguiente screen es un avatar'),
-            trailing: Icon(Icons.navigate_next),
+            trailing: Icon(Icons.navigate_next, color: Colors.red, size: 40,),
+            onTap: () => Navigator.pushNamed(context, 'avatar'),
           ),
           ListTile(
-            leading: Icon(Icons.movie),
+            leading: Icon(Icons.movie, color: Colors.amber),
             title: Text('Movie Screen'),
             subtitle: Text('El siguiente screen es el app movie'),
-            trailing: Icon(Icons.navigate_next),
+            trailing: Icon(Icons.navigate_next, color: Colors.red , size: 40,),
+            onTap: () => Navigator.pushNamed(context, 'movie'),
           ),
         ],
       ),
