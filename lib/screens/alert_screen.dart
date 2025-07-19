43a6d32586423aae7aff692bca6fe291cbba4d6d
Sadot18 <19161291@itoaxaca.edu.mx>
2025-07-18 23:13:44 -0600

    Actualiza AlertScreen para mostrar una imagen con FadeInImage en lugar de texto

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,11 +3,22 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({super.key});
 
+  //https://cdn.pixabay.com/photo/2024/05/22/21/51/dog-8781844_960_720.jpg
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Alert Screen")),
-      body: Center(child: Text('This is the Alert screen!')),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.only(left: 50, right: 50),
+          child: FadeInImage(
+            placeholder: AssetImage('assets/no-image.jpg'),
+            image: NetworkImage(
+              "https://cdn.pixabay.com/photo/2024/05/22/21/51/dog-8781844_960_720.jpg",
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
