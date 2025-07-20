b48d8a9d0258e7a698e5637c79936048d939f5dd
Sadot18 <19161291@itoaxaca.edu.mx>
2025-07-19 20:19:40 -0600

    Reemplaza el ListView estático en CustomHomePage por CustomListView

--- lib/screens/custom_home_page.dart
+++ lib/screens/custom_home_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_widgets/widgets/custom_list_view.dart';
 import 'package:flutter/material.dart';
 
 class CustomHomePage extends StatelessWidget {
@@ -14,38 +15,7 @@ class CustomHomePage extends StatelessWidget {
           style: TextStyle(fontSize: 24, color: Colors.indigo),
         ),
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            leading: Icon(Icons.list, color: Colors.blue),
-            title: Text('ListView Screen'),
-            subtitle: Text('El siguiente screen es un listview'),
-            trailing: Icon(Icons.navigate_next, color: Colors.red, size: 40,),
-            onTap: () => Navigator.pushNamed(context, 'listview'),
-          ),
-          ListTile(
-            leading: Icon(Icons.notification_add, color: Colors.red),
-            title: Text('Alert Screen'),
-            subtitle: Text('El siguiente screen es un alert'),
-            trailing: Icon(Icons.navigate_next, color: Colors.red, size: 40,),
-            onTap: () => Navigator.pushNamed(context, 'alert'),
-          ),
-          ListTile(
-            leading: Icon(Icons.supervised_user_circle_outlined, color: Colors.green),
-            title: Text('Avatar Screen'),
-            subtitle: Text('El siguiente screen es un avatar'),
-            trailing: Icon(Icons.navigate_next, color: Colors.red, size: 40,),
-            onTap: () => Navigator.pushNamed(context, 'avatar'),
-          ),
-          ListTile(
-            leading: Icon(Icons.movie, color: Colors.amber),
-            title: Text('Movie Screen'),
-            subtitle: Text('El siguiente screen es el app movie'),
-            trailing: Icon(Icons.navigate_next, color: Colors.red , size: 40,),
-            onTap: () => Navigator.pushNamed(context, 'movie'),
-          ),
-        ],
-      ),
+      body: CustomListView(),
     );
   }
 }
