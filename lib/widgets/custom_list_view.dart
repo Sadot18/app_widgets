104aad8adb8253cbb53838404197169524370282
Sadot18 <19161291@itoaxaca.edu.mx>
2025-07-20 19:47:38 -0600

    Refactor CustomListView para utilizar ListView.builder y cargar opciones de menú dinámicamente

--- lib/widgets/custom_list_view.dart
+++ lib/widgets/custom_list_view.dart
@@ -1,44 +1,23 @@
-
+import 'package:app_widgets/models/menu_option.dart';
+import 'package:app_widgets/routes/routes.dart';
 import 'package:flutter/material.dart';
 
 class CustomListView extends StatelessWidget {
-  const CustomListView({
-    super.key,
-  });
+  CustomListView({super.key});
+
+  final List<MenuOption> option = Routes.option;
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        ListTile(
-          leading: Icon(Icons.list, color: Colors.blue),
-          title: Text('ListView Screen'),
-          subtitle: Text('El siguiente screen es un listview'),
-          trailing: Icon(Icons.navigate_next, color: Colors.red, size: 40,),
-          onTap: () => Navigator.pushNamed(context, 'listview'),
+    return ListView.builder(
+      itemCount: option.length,
+      itemBuilder: (context, index) => ListTile(
+          leading: Icon( option[index].icono),
+          title: Text(option[index].title),
+          subtitle: Text(option[index].description),
+          trailing: Icon(Icons.navigate_next, color: Colors.red, size: 40),
+          onTap: () => Navigator.pushNamed(context, option[index].route),
         ),
-        ListTile(
-          leading: Icon(Icons.notification_add, color: Colors.red),
-          title: Text('Alert Screen'),
-          subtitle: Text('El siguiente screen es un alert'),
-          trailing: Icon(Icons.navigate_next, color: Colors.red, size: 40,),
-          onTap: () => Navigator.pushNamed(context, 'alert'),
-        ),
-        ListTile(
-          leading: Icon(Icons.supervised_user_circle_outlined, color: Colors.green),
-          title: Text('Avatar Screen'),
-          subtitle: Text('El siguiente screen es un avatar'),
-          trailing: Icon(Icons.navigate_next, color: Colors.red, size: 40,),
-          onTap: () => Navigator.pushNamed(context, 'avatar'),
-        ),
-        ListTile(
-          leading: Icon(Icons.movie, color: Colors.amber),
-          title: Text('Movie Screen'),
-          subtitle: Text('El siguiente screen es el app movie'),
-          trailing: Icon(Icons.navigate_next, color: Colors.red , size: 40,),
-          onTap: () => Navigator.pushNamed(context, 'movie'),
-        ),
-      ],
     );
   }
 }
